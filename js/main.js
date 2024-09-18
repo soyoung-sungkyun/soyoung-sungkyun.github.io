cc2fee5964a66dd907d2c3aa3d898cf812f7c19c
류성균[파이낸셜인텔리전스] <sk.rhyeu@gmail.com>
2024-09-18 13:08:56 +0900

    카운트다운 날짜 수정

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("May 20, 2023 13:00:00").getTime();
+		var countDownDate = new Date("Jan 18, 2025 16:40:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
